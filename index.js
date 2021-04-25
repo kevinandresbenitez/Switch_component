c24649685cb6f0d14bb149ea1dc856abb57c3019
Kevin Benitez <kevinescudoderoble@gmail.com>
2021-04-25 16:49:12 -0300

    V.1

---
+++ index.js
@@ -0,0 +1,17 @@
+import './index.css';
+
+export default function Switch (props){
+
+
+    return (
+
+        <label class="switch">
+            <input class="switch_input" type="checkbox" />
+            <samp class="switch_radio"></samp>
+        </label>
+
+
+
+    )
+
+}
